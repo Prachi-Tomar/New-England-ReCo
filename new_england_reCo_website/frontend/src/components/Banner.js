a12d3b5141f871e6e9f395c597bb5a4e030906f5
Prachi-Tomar <79598582+Prachi-Tomar@users.noreply.github.com>
2024-01-17 19:21:19 -0500

    working banner

--- new_england_reCo_website/frontend/src/components/Banner.js
+++ new_england_reCo_website/frontend/src/components/Banner.js
@@ -1,9 +1,27 @@
 import React from "react";
 
+const bannerStyle = {
+    background: '#005030',
+    height: '300px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    textAlign: 'center',
+    color: 'white',
+};
+
+const headingStyle = {
+    fontSize: '3em',
+    marginBottom: '0.5em',
+    fontFamily: '"EB Garamond", serif',
+    color: 'white'
+}; 
+
 function Banner() {
     return (
-        <div className="banner">
-            <h1>The New England Real Estate Company</h1>
+        <div className="banner" style={bannerStyle}>
+            <h1 style={headingStyle}>The New England Real Estate Company</h1>
         </div>
     );
 }
--- new_england_reCo_website/frontend/src/components/HomePage.js
+++ new_england_reCo_website/frontend/src/components/HomePage.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Banner from "./Banner";
+import ImageGallery from "./ImageGallery";
 
 function HomePage() {
-    return (<><h1>The New England Real Estate Company</h1>
-    <img
-      component="img"
-      alt="The house from the offer."
-      src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2" /></>)
+    return (
+      <div className="homePage" align="center">
+        <Banner />
+        <ImageGallery />
+      </div>
+   )
 }
 
 export default HomePage;
